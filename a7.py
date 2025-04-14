bcb5d559525cfb7dc46656c57c60c3a58474b8f8
Robert Berg <rberg@cps.edu>
2025-04-14 11:12:29 -0500

    finished classify

--- a7.py
+++ a7.py
@@ -146,20 +146,32 @@ class BayesClassifier:
         pos_denominator = sum(self.pos_freqs.values())
         neg_denominator = sum(self.neg_freqs.values())
 
+        # print(pos_denominator, neg_denominator)
+
         # for each token in the text, calculate the probability of it occurring in a
         # postive document and in a negative document and add the logs of those to the
         # running sums. when calculating the probabilities, always add 1 to the numerator
         # of each probability for add one smoothing (so that we never have a probability
         # of 0)
+        for token in tokens:
+            pos_freqs = self.pos_freqs.get(token, 0) + 1
+            neg_freqs = self.neg_freqs.get(token, 0) + 1
+            print(pos_freqs, neg_freqs)
 
+            pos_score += math.log(pos_freqs / pos_denominator)
+            neg_score += math.log(neg_freqs / neg_denominator)
 
+            print(pos_score, neg_score)
         # for debugging purposes, it may help to print the overall positive and negative
         # probabilities
         
 
         # determine whether positive or negative was more probable (i.e. which one was
         # larger)
-        
+        if pos_score > neg_score:
+            return "positive"
+        else:
+            return "negative"
 
         # return a string of "positive" or "negative"
 
@@ -293,8 +305,12 @@ if __name__ == "__main__":
     # uncomment the below lines once you've implemented `classify`
     print("\nThe following should all be positive.")
     print(b.classify('I love computer science'))
-    # print(b.classify('this movie is fantastic'))
-    # print("\nThe following should all be negative.")
-    # print(b.classify('rainy days are the worst'))
-    # print(b.classify('computer science is terrible'))
+    print(b.classify('this movie is fantastic'))
+    print("\nThe following should all be negative.")
+    print(b.classify('rainy days are the worst'))
+    print(b.classify('computer science is terrible'))
+
+    print()
+    print(b.classify("intro to artificial intelligence is the best class"))
+    print(b.classify("not the best way to do this"))
     pass
